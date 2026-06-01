fb50040371cbf37b5cc9f11617260bf48348b34a
Meiryanne Martins <meirympereira@gmail.com>
2026-06-01 19:25:40 -0300

    feat: setup project

---
+++ products_api/app.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI, status
+from products_api.routers import products
+
+app = FastAPI()
+
+app.include_router(
+    router = products.router,
+    prefix='/api/v1/products',
+    tags=['products'],
+)
+
+@app.get('/health_check', status_code = status.HTTP_200_OK)
+def health_check():
+    return {'status': 'ok'}
---
+++ products_api/routers/products.py
@@ -0,0 +1,23 @@
+from fastapi import APIRouter, status
+
+router = APIRouter()
+
+@router.get('/', status_code = status.HTTP_200_OK)
+async def list_products():
+    return {
+        'products': [
+            {
+                'id': 1,
+                'name': 'Logitech MX Keys',
+            },
+            {
+                'id': 2,
+                'name': 'RTX 5070',
+            },
+            {
+                'id': 3,
+                'name': 'SSD 1TB',
+            },
+        ]
+
+    }
