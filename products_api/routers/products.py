10251c1ca129ed141877428b24aa3d8193c0e163
Meiryanne Martins <meirympereira@gmail.com>
2026-06-01 22:21:14 -0300

    feat: add endpoint - list products

--- products_api/routers/products.py
+++ products_api/routers/products.py
@@ -1,23 +1,35 @@
 from fastapi import APIRouter, status
 
+from products_api.schemas.products import ProductListPublicSchema
+
 router = APIRouter()
 
 
-@router.get('/', status_code=status.HTTP_200_OK)
+@router.get(
+    path='/',
+    status_code=status.HTTP_200_OK,
+    response_model=ProductListPublicSchema,
+)
 async def list_products():
     return {
         'products': [
             {
                 'id': 1,
                 'name': 'Logitech MX Keys',
+                'price': 729.90,
+                'description': 'Bom!',
             },
             {
                 'id': 2,
                 'name': 'RTX 5070',
+                'price': 1290.90,
+                'description': 'Bom!',
             },
             {
                 'id': 3,
                 'name': 'SSD 1TB',
+                'price': 429.90,
+                'description': 'Bom!',
             },
         ]
     }
